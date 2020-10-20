d0fbd484996bcd05c0367a89ca96b0d25c3b01f2
samirsaliba <samirtsj@gmail.com>
2020-10-20 14:39:41 -0300

    Fix de email unico

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -5,9 +5,7 @@ from django.contrib.auth.models import User
 
 from rest_framework import serializers
 
-User._meta.get_field('email')._unique = True
-
-
+User._meta.get_field('email')._unique = False
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
